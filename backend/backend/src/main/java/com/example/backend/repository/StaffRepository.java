54f13f58172c4802f1bd09befe14f018edb4f0fe
losikilove <75561119+losikilove@users.noreply.github.com>
2023-12-12 09:59:58 +0700

    add repository annotations in each repository files

--- backend/backend/src/main/java/com/example/backend/repository/StaffRepository.java
+++ backend/backend/src/main/java/com/example/backend/repository/StaffRepository.java
@@ -1,9 +1,11 @@
 package com.example.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.backend.entity.Staff;
 
+@Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
 
 }
--- backend/backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.backend.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
 }
