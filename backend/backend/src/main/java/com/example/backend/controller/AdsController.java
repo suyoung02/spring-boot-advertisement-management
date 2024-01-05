cae128a14a17a2d821d73fd1c1db6fb899767c23
losikilove <75561119+losikilove@users.noreply.github.com>
2024-01-05 20:03:32 +0700

    change the data type of image in REPORT table to varchar

--- backend/backend/src/main/java/com/example/backend/controller/AdsController.java
+++ backend/backend/src/main/java/com/example/backend/controller/AdsController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,16 +22,16 @@ public class AdsController {
     private final AdsService adsService;
 
     @GetMapping("/all/getAllPosition")
-    public ResponseEntity<List<AdsPosition>> getAllPosition(){
+    public ResponseEntity<List<AdsPosition>> getAllPosition() {
         System.out.println(1);
         List<AdsPosition> result = adsService.getAllPosition();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/all/getDetailPosition/{id}")
-    public ResponseEntity<?> getDetailPosition(@PathVariable(value = "id") Integer id){
+    public ResponseEntity<?> getDetailPosition(@PathVariable(value = "id") Integer id) {
         Optional<AdsPosition> position = adsService.getDetailPosition(id);
-        if(position != null){
+        if (position != null) {
             return new ResponseEntity<>(position, HttpStatus.OK);
         }
         return new ResponseEntity<>("Id Not found", HttpStatus.NOT_FOUND);
@@ -50,86 +49,85 @@ public class AdsController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
     @DeleteMapping("/vhtt/deletePosition/{id}")
-    public ResponseEntity<String> deletePosition(@PathVariable(value = "id") Integer id){
-        try{
+    public ResponseEntity<String> deletePosition(@PathVariable(value = "id") Integer id) {
+        try {
             System.out.println(id);
-            if(adsService.deletePosition(id)){
+            if (adsService.deletePosition(id)) {
                 return new ResponseEntity<>("Deleted", HttpStatus.OK);
             }
             return new ResponseEntity<>("Id not found", HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
         }
     }
 
     @PutMapping("/vhtt/updatePosition/{id}")
-    public ResponseEntity<AdsPosition> updatePosition(@PathVariable(value = "id") Integer id, @Valid @RequestBody AddPositionRequest newPosition){
-        try{
+    public ResponseEntity<AdsPosition> updatePosition(@PathVariable(value = "id") Integer id,
+            @Valid @RequestBody AddPositionRequest newPosition) {
+        try {
             AdsPosition result = adsService.updatePosition(id, newPosition);
             return new ResponseEntity<>(result, HttpStatus.OK);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    //Panel controller....................
+    // Panel controller....................
 
     @GetMapping("/all/getAllPanel")
-    public ResponseEntity<List<AdsPanel>> getAllPanels(){
+    public ResponseEntity<List<AdsPanel>> getAllPanels() {
         List<AdsPanel> ads = adsService.getAllPanels();
         System.out.println(ads);
         return new ResponseEntity<>(ads, HttpStatus.OK);
     }
 
     @GetMapping("/all/getDetailPanel/{id}")
-    public ResponseEntity<?> getDetailPanel(@PathVariable(value = "id") Integer id){
+    public ResponseEntity<?> getDetailPanel(@PathVariable(value = "id") Integer id) {
         Optional<AdsPanel> panel = adsService.getDetailPanel(id);
-        if(panel != null){
+        if (panel != null) {
             return new ResponseEntity<>(panel, HttpStatus.OK);
         }
         return new ResponseEntity<>("Id Not found", HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/vhtt/addNewPanel")
-    public ResponseEntity<String> addNewPanel(@Valid @RequestBody AddPanelRequest newPanel){
+    public ResponseEntity<String> addNewPanel(@Valid @RequestBody AddPanelRequest newPanel) {
         System.out.println(newPanel);
-        try{
-            if(adsService.addNewPanel(newPanel)){
+        try {
+            if (adsService.addNewPanel(newPanel)) {
                 return new ResponseEntity<>("panel", HttpStatus.OK);
             }
             return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/vhtt/deletePanel")
-    public ResponseEntity<String> deletePanel(@PathVariable(value = "id") Integer id){
-        try{
-            if(adsService.deletePanel(id)){
+    public ResponseEntity<String> deletePanel(@PathVariable(value = "id") Integer id) {
+        try {
+            if (adsService.deletePanel(id)) {
                 return new ResponseEntity<>("Deleted", HttpStatus.OK);
             }
             return new ResponseEntity<>("Id not found", HttpStatus.NOT_FOUND);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
         }
     }
+
     @PutMapping("/vhtt/updatePanel")
-    public ResponseEntity<AdsPanel> updatePanel(@PathVariable(value = "id") Integer id, @Valid @RequestBody AddPanelRequest newPosition){
-        try{
+    public ResponseEntity<AdsPanel> updatePanel(@PathVariable(value = "id") Integer id,
+            @Valid @RequestBody AddPanelRequest newPosition) {
+        try {
             AdsPanel result = adsService.updatePanel(id, newPosition);
             return new ResponseEntity<>(result, HttpStatus.OK);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
