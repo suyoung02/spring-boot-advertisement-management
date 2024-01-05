6f0f3017347b25273577de7f3571d60f34137636
HuyDeveloper <vinhhuy16072002@gmail.com>
2024-01-05 11:24:39 +0700

    Add api doc

--- backend/backend/src/main/java/com/example/backend/controller/AdsController.java
+++ backend/backend/src/main/java/com/example/backend/controller/AdsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/ads")
 public class AdsController {
